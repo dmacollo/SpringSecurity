215e51d0dcb951bdf27cdfe5741eaf9626e89e1b
Don Collo <Don Collo@SIKSIKLAT>
2017-01-15 21:40:17 -0800

    Updates

--- SpringSecurityGit/SpringSecurity/src/main/java/ae/ih/main/SpringSecurityApplication.java
+++ SpringSecurityGit/SpringSecurity/src/main/java/ae/ih/main/SpringSecurityApplication.java
@@ -1,12 +1,24 @@
 package ae.ih.main;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+@ComponentScan({"ae.ih"})
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringSecurityApplication.class, args);
+		String[] beanNames=context.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String bean:beanNames){
+			System.out.println(bean);
+		}
 	}
 }
--- SpringSecurityGit/SpringSecurity/src/test/java/ae/ih/SpringSecurityApplicationTests.java
+++ SpringSecurityGit/SpringSecurity/src/test/java/ae/ih/SpringSecurityApplicationTests.java
@@ -1,16 +1,39 @@
 package ae.ih;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import ae.ih.config.SystemConfig;
+import ae.ih.domain.DataSource;
+import ae.ih.domain.JmsBroker;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=SystemConfig.class)
+@ActiveProfiles({"qa"})
 public class SpringSecurityApplicationTests {
-
+	
+	@Autowired
+	JmsBroker jmsBroker;
+	@Autowired
+	DataSource dataSource;
+	
 	@Test
-	public void contextLoads() {
+	public void testJmsConnection() throws Exception{
+		assertEquals("localhost", jmsBroker.getJmsServer());
+	}	
+	
+	@Test
+	public void testDataSource(){
+		System.out.println(dataSource.getURL());
 	}
-
 }
